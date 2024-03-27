2e778445a7bf1528655d95ac11eb24f2af245a36
Pratyush R <pratyush.r8324@gmail.com>
2024-03-28 01:11:43 +0530

    Added connection string, with example

--- Assignment3/user_template.vb
+++ Assignment3/user_template.vb
@@ -1,6 +1,25 @@
-﻿Public Class user_template
+﻿Imports System.Configuration
+Imports Microsoft.Data.SqlClient
+Public Class user_template
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Example of how to connect to Database using connection string in app.config file.
+        'Needs Microsoft.Data.SqlClient.
+        'If not present, you need to get it through NuGet package manager
+        'Access the same through context menu by clicking on project name.
+        'If this is not available locally, you need to get it online.
+        'For that you need to add nuget.org as source in Tools->NuGet...
+        'Ask GPT for details.
+        'As it stands, the System.Data.SqlClient does not work on .NET 5+ or .NET core (I assume default installed will be 8.0)
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("MyConnectionString").ConnectionString
+        Using connection As New SqlConnection(connectionString)
+            Try
+                connection.Open()s
+                MessageBox.Show("Connection successful!")
 
+            Catch ex As Exception
+                MessageBox.Show("Error connecting to database: " & ex.Message)
+            End Try
+        End Using
     End Sub
 
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
