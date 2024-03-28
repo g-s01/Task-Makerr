aa700ac472bc136b63fe21b43dfe5d01883a7c17
Divya <divyagarg4747@gmail.com>
2024-03-28 22:11:06 +0530

    added appointment pages

---
+++ Assignment3/user_appointment_details.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Configuration
+Imports Microsoft.Data.SqlClient
+Public Class user_appointment_details
+    Private Sub user_appointment_details_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("MyConnectionString").ConnectionString
+        Using connection As New SqlConnection(connectionString)
+            Try
+                connection.Open()
+                MessageBox.Show("Connection successful!")
+
+            Catch ex As Exception
+                MessageBox.Show("Error connecting to database: " & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+End Class
