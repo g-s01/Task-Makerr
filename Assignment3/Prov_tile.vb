4c71b0b77b6dd86a461c486d1a998adefa843f9d
mit-riya <m.riya@iitg.ac.in>
2024-03-29 23:33:50 +0530

    changes in prov_tile

--- Assignment3/Prov_tile.vb
+++ Assignment3/Prov_tile.vb
@@ -43,4 +43,80 @@ Public Class Prov_tile
         toolTip1.SetToolTip(Label2, Label2.Text)
     End Sub
 
+    Friend WithEvents PictureBox1 As PictureBox
+
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        PictureBox2 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.prov
+        PictureBox1.Location = New Point(27, 5)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(97, 97)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.Location = New Point(16, 185)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(75, 38)
+        Label1.TabIndex = 1
+        Label1.Text = "Label1"
+        ' 
+        ' Label2
+        ' 
+        Label2.Location = New Point(44, 124)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(80, 40)
+        Label2.TabIndex = 2
+        Label2.Text = "Label2"
+        ' 
+        ' Label3
+        ' 
+        Label3.Location = New Point(117, 185)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(53, 20)
+        Label3.TabIndex = 3
+        Label3.Text = "Label3"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = My.Resources.Resources.Star
+        PictureBox2.Location = New Point(143, 185)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(27, 34)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 4
+        PictureBox2.TabStop = False
+        ' 
+        ' Prov_tile
+        ' 
+        AutoSize = True
+        Controls.Add(PictureBox2)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Name = "Prov_tile"
+        Size = New Size(173, 223)
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox2 As PictureBox
 End Class
