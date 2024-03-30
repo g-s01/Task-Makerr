de6e6b7b3817bdc330d64670f4df51e7a3d9a60c
Abhinav <aabhikumar2003@gmail.com>
2024-03-30 12:28:42 +0530

    Backend for login page

--- Assignment3/Module_global.vb
+++ Assignment3/Module_global.vb
@@ -1,6 +1,6 @@
 ﻿Module Module_global
     ' global variables to encourage information flow between different forms
-    Public User_ID As String = ""
-    Public Provider_ID As String = ""
+    Public User_ID As Integer = -1
+    Public Provider_ID As Integer = -1
     Public Email As String = ""
 End Module
--- Assignment3/My Project/Application.Designer.vb
+++ Assignment3/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Assignment3.UserHome
+            Me.MainForm = Global.Assignment3.Login
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
