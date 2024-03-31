9e9e02b23c3e52b95ceaa00472f8192f9de5739a
botketan <ketansingh918@gmail.com>
2024-03-31 23:01:09 +0530

    Required Changes for UserTemplate

--- Assignment3/My Project/Application.Designer.vb
+++ Assignment3/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Assignment3.UserHome
+            Me.MainForm = Global.Assignment3.user_template
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
