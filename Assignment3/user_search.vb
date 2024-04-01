342c471275c03c1fbf4642f03b54da70a972bba4
Sarvesh <gholap@iitg.ac.in>
2024-04-01 16:51:18 +0530

    User search by name implemented

--- Assignment3/user_search.vb
+++ Assignment3/user_search.vb
@@ -1,12 +1,18 @@
 ﻿Imports System.Configuration
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
 Imports Microsoft.Data.SqlClient
 Public Class user_search
+
+
+    Dim reviews As New Dictionary(Of Int32, Tuple(Of Integer, Integer))()
+    Dim rating_prov As New Dictionary(Of Int32, Double)()
 
     ' Define a global array to store providers
     Dim providers As New List(Of Entry)
 
     ' Define a structure to hold provider details
     Structure Entry
+        Public providerID As Integer
         Public providerName As String
         Public service As String
         Public location As String
@@ -20,12 +26,31 @@ Public Class user_search
         Using connection As New SqlConnection(connectionString)
             Try
                 connection.Open()
-                MessageBox.Show("Connection successful!")
-
+                Dim command_rev As New SqlCommand("SELECT review.*, deals.* FROM review INNER JOIN deals ON review.deal_id = deals.deal_id", connection)
+                Using reader As SqlDataReader = command_rev.ExecuteReader()
+                    While reader.Read()
+                        Dim provider As Int32 = reader.GetInt32(reader.GetOrdinal("provider_id"))
+                        Dim rate As String = reader.GetInt32(reader.GetOrdinal("rating"))
+                        If reviews.ContainsKey(provider) Then
+                            Dim currentValue As Tuple(Of Integer, Integer) = reviews(provider)
+                            Dim newValue As New Tuple(Of Integer, Integer)(currentValue.Item1 + rate, currentValue.Item2 + 1)
+                            reviews(provider) = newValue
+                        Else
+                            reviews.Add(provider, New Tuple(Of Integer, Integer)(rate, 1))
+                        End If
+                    End While
+                End Using
+                For Each pair As KeyValuePair(Of Int32, Tuple(Of Integer, Integer)) In reviews
+                    Dim currentValue As Tuple(Of Integer, Integer) = pair.Value
+                    Dim rating As Double
+                    rating = Math.Round(currentValue.Item1 / CType(currentValue.Item2, Double), 1)
+                    rating_prov.Add(pair.Key, rating)
+                Next
             Catch ex As Exception
                 MessageBox.Show("Error connecting to database: " & ex.Message)
             End Try
         End Using
+        LoadProviders()
         PopulateTable()
     End Sub
 
@@ -48,7 +73,7 @@ Public Class user_search
     Private Sub PopulateTable()
 
         providersTablePanel.Controls.Clear()
-        LoadProviders()
+        'LoadProviders()
 
         ' Add borrowedBooks to the table
         For rowIndex As Integer = 0 To providers.Count - 1
@@ -102,15 +127,53 @@ Public Class user_search
 
     Private Sub LoadProviders()
         providers.Clear()
-        providers.Add(New Entry With {.providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
-        providers.Add(New Entry With {.providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
-        providers.Add(New Entry With {.providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
-        providers.Add(New Entry With {.providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
-        providers.Add(New Entry With {.providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
-        providers.Add(New Entry With {.providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
-        providers.Add(New Entry With {.providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
-        providers.Add(New Entry With {.providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
+        providers.Add(New Entry With {.providerID = 123, .providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
+        providers.Add(New Entry With {.providerID = 123, .providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
+        providers.Add(New Entry With {.providerID = 123, .providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
+        providers.Add(New Entry With {.providerID = 123, .providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
+        providers.Add(New Entry With {.providerID = 123, .providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
+        providers.Add(New Entry With {.providerID = 123, .providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
+        providers.Add(New Entry With {.providerID = 123, .providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
+        providers.Add(New Entry With {.providerID = 123, .providerName = "testProvider", .service = "testService", .location = "testLocation", .cost = 100, .rating = 4.4, .RadioButton = New RadioButton()})
 
     End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or TextBox1.Text = "Search for providers by name" Then
+            MessageBox.Show("Enter a text to search")
+        Else
+            providers.Clear()
+            Dim connectionString As String = ConfigurationManager.ConnectionStrings("MyConnectionString").ConnectionString
+            Using connection As New SqlConnection(connectionString)
+                Try
+                    connection.Open()
+                    Dim command As New SqlCommand("SELECT * FROM provider WHERE providername like '" & TextBox1.Text & "'", connection)
+                    Using reader As SqlDataReader = command.ExecuteReader()
+                        ' Loop through the SqlDataReader
+                        While reader.Read()
+                            ' Get the values of the current row
+                            Dim service As String = reader.GetString(reader.GetOrdinal("service"))
+                            'Dim loc As String = reader.GetString(reader.GetOrdinal("location"))
+                            Dim name As String = reader.GetString(reader.GetOrdinal("providername"))
+                            Dim provider As Int32 = reader.GetInt32(reader.GetOrdinal("provider_id"))
+                            Dim cost As Double = reader.GetInt32(reader.GetOrdinal("cost_per_hour"))
+                            Dim rating As String
+                            If reviews.ContainsKey(provider) Then
+                                Dim currentValue As Tuple(Of Integer, Integer) = reviews(provider)
+                                rating = rating_prov(provider).ToString()
+                            Else
+                                rating = "N/A"
+                            End If
+                            providers.Add(New Entry With {.providerID = provider, .providerName = name, .service = service, .location = "loc", .cost = cost, .rating = rating, .RadioButton = New RadioButton()})
+                        End While
+                    End Using
+
+                Catch ex As Exception
+                    MessageBox.Show("Error connecting to database: " & ex.Message)
+                End Try
+            End Using
+            PopulateTable()
+        End If
+
+    End Sub
 End Class
