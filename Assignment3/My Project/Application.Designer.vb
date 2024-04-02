f84248db01f3fd4b1d22f5ded8570738324232f2
Raghuveer22 <posa.mokshith@iitg.ac.in>
2024-04-02 20:58:03 +0530

    for pr

--- Assignment3/My Project/Application.Designer.vb
+++ Assignment3/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Assignment3.user_appointment_details
+            Me.MainForm = Global.Assignment3.Landing
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
