db6c4b2f19c2f294a26c67e733b7a6aaacc0eff6
saisanthoshbussa <118352633+saisanthoshbussa@users.noreply.github.com>
2024-04-14 13:33:57 +0530

    Feedbackform update

--- Assignment3/FeedbackForm.Designer.vb
+++ Assignment3/FeedbackForm.Designer.vb
@@ -44,23 +44,25 @@ Partial Class FeedbackForm
         ' 
         lblTitle.AutoSize = True
         lblTitle.Font = New Font("Microsoft YaHei", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        lblTitle.Location = New Point(187, 60)
+        lblTitle.Location = New Point(207, 60)
         lblTitle.Margin = New Padding(4, 0, 4, 0)
         lblTitle.Name = "lblTitle"
         lblTitle.Size = New Size(480, 39)
         lblTitle.TabIndex = 0
         lblTitle.Text = "We Would Love Some Feedback"
+        lblTitle.TextAlign = ContentAlignment.TopCenter
         ' 
         ' lblRateUs
         ' 
         lblRateUs.AutoSize = True
         lblRateUs.Font = New Font("Microsoft YaHei", 16.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        lblRateUs.Location = New Point(246, 120)
+        lblRateUs.Location = New Point(262, 120)
         lblRateUs.Margin = New Padding(4, 0, 4, 0)
         lblRateUs.Name = "lblRateUs"
         lblRateUs.Size = New Size(348, 36)
         lblRateUs.TabIndex = 1
         lblRateUs.Text = "How would you Rate Us?"
+        lblRateUs.TextAlign = ContentAlignment.TopCenter
         ' 
         ' txtFeedback
         ' 
@@ -110,7 +112,7 @@ Partial Class FeedbackForm
         ' star5
         ' 
         star5.Image = My.Resources.Resources.bg_star
-        star5.Location = New Point(514, 191)
+        star5.Location = New Point(492, 191)
         star5.Name = "star5"
         star5.Size = New Size(62, 52)
         star5.SizeMode = PictureBoxSizeMode.AutoSize
@@ -121,7 +123,7 @@ Partial Class FeedbackForm
         ' 
         star4.Image = My.Resources.Resources.bg_star
         star4.InitialImage = My.Resources.Resources.bg_star
-        star4.Location = New Point(446, 191)
+        star4.Location = New Point(424, 191)
         star4.Name = "star4"
         star4.Size = New Size(62, 52)
         star4.SizeMode = PictureBoxSizeMode.AutoSize
@@ -131,7 +133,7 @@ Partial Class FeedbackForm
         ' star3
         ' 
         star3.Image = My.Resources.Resources.bg_star
-        star3.Location = New Point(378, 191)
+        star3.Location = New Point(356, 191)
         star3.Name = "star3"
         star3.Size = New Size(62, 52)
         star3.SizeMode = PictureBoxSizeMode.AutoSize
@@ -141,7 +143,7 @@ Partial Class FeedbackForm
         ' star2
         ' 
         star2.Image = My.Resources.Resources.bg_star
-        star2.Location = New Point(310, 191)
+        star2.Location = New Point(288, 191)
         star2.Name = "star2"
         star2.Size = New Size(62, 52)
         star2.SizeMode = PictureBoxSizeMode.AutoSize
@@ -151,7 +153,7 @@ Partial Class FeedbackForm
         ' star1
         ' 
         star1.Image = My.Resources.Resources.bg_star
-        star1.Location = New Point(242, 191)
+        star1.Location = New Point(220, 191)
         star1.Name = "star1"
         star1.Size = New Size(62, 52)
         star1.SizeMode = PictureBoxSizeMode.AutoSize
