9fe1ef87d0e08e4fa30924dbe915fa83f18fd462
DevDev <devikas.iitg@gmail.com>
2024-04-14 17:38:20 +0530

    partial frontend

--- Assignment3/My Project/Application.Designer.vb
+++ Assignment3/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Assignment3.Landing
+            Me.MainForm = Global.Assignment3.provider_info
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
